95fdb502c955f50354b4732328bbe25796117fc4
shizi <15139618+shizimao@user.noreply.gitee.com>
2024-11-13 16:27:03 +0800

    :0012文件提交

---
+++ 0012ds/files/添加打包产品/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r,s,t],u,_(v,w,x,y,g,z,A,_(),B,[],C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,G,G,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(I,J,K,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,K,_(bk,bl,bm,bl,bn,bl,bo,bp)),i,_(j,k,l,k)),bq,_(),br,_(),bs,_(bt,[])),bu,_(),bv,_());}; 
+var b="url",c="添加打包产品.html",d="generationDate",e=new Date(1731485727400.56),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="productType",r="OrderState",s="BackState",t="message_model",u="page",v="packageId",w="999e249f8c1f43c08132af55917e1bdf",x="type",y="Axure:Page",z="添加打包产品",A="notes",B="annotations",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="near",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="r",bl=0,bm="g",bn="b",bo="a",bp=0.349019607843137,bq="adaptiveStyles",br="interactionMap",bs="diagram",bt="objects",bu="masters",bv="objectPaths";
+return _creator();
+})());
